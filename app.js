2b79f9b36f6047aa202afc7b6a7a3f235bc61d1c
Destroyerofdarkness <dawidadogier@gmail.com>
2025-12-11 08:48:31 +0100

    Added a description of the creating vm page and added css to the section with class name description

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require("express");
 
 const app = express();
 
-const router = require("./router/default_routes.js")
+const router = require("./router/default_routes.js");
 
-const path = require("path")
+const path = require("path");
 
 app.set("view engine", "ejs");
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000")
-})
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 
-app.use(router)
+app.use(router);
